bc1267e9307aecc630531ec650ea02b3ca42b792
RobsBuddySystem <robsbuddysystem@users.noreply.github.com>
2026-07-19 12:22:44 +0200

    2026-07-19: fan account type -- login.html Fan tab (passwordless magic-link), itinerary.html dashboard gains Following + account email, homepage fan CTA

--- comedy-atlas/sw.js
+++ comedy-atlas/sw.js
@@ -23,7 +23,7 @@
  */
 "use strict";
 
-var CACHE_VERSION = "atlas-2f45b6635129";
+var CACHE_VERSION = "atlas-cf088d81793b";
 var STATIC_CACHE = "atlas-static-" + CACHE_VERSION;
 var DATA_CACHE = "atlas-data-" + CACHE_VERSION;
 
