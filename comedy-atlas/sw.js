e8403096f489d37f6e61a027562cecab36959498
RobsBuddySystem <robsbuddysystem@users.noreply.github.com>
2026-07-19 18:07:22 +0200

    2026-07-19: comedy-atlas frontend polish -- hamburger mobile nav (#16), PWA install hint (#15), near-me distance sort on city pages (#14), venue-correction feedback context (#17)

--- comedy-atlas/sw.js
+++ comedy-atlas/sw.js
@@ -23,7 +23,7 @@
  */
 "use strict";
 
-var CACHE_VERSION = "atlas-75bec8fb7bed";
+var CACHE_VERSION = "atlas-626bb1adbea1";
 var STATIC_CACHE = "atlas-static-" + CACHE_VERSION;
 var DATA_CACHE = "atlas-data-" + CACHE_VERSION;
 
