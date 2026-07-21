6bae85bd1e50f93d34fcbb462937425c9fbe1660
RobsBuddySystem <robsbuddysystem@users.noreply.github.com>
2026-07-21 15:07:41 +0200

    2026-07-21: Comics nav link live on homepage (+ SW bump)

--- comedy-atlas/sw.js
+++ comedy-atlas/sw.js
@@ -23,7 +23,7 @@
  */
 "use strict";
 
-var CACHE_VERSION = "atlas-6a5424cf8576";
+var CACHE_VERSION = "atlas-0495a4aef530";
 var STATIC_CACHE = "atlas-static-" + CACHE_VERSION;
 var DATA_CACHE = "atlas-data-" + CACHE_VERSION;
 
