57a68e4822cbd3166afda8b8ca7273e4d80d186e
RobsBuddySystem <robsbuddysystem@users.noreply.github.com>
2026-07-25 21:38:04 +0200

    2026-07-25: fix password-toggle CSS bug + SW bump

--- comedy-atlas/sw.js
+++ comedy-atlas/sw.js
@@ -23,7 +23,7 @@
  */
 "use strict";
 
-var CACHE_VERSION = "atlas-f4df0a13c3a8";
+var CACHE_VERSION = "atlas-cssfix25725aa";
 var STATIC_CACHE = "atlas-static-" + CACHE_VERSION;
 var DATA_CACHE = "atlas-data-" + CACHE_VERSION;
 
