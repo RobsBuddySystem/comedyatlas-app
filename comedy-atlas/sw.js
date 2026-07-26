8f03ce20b7f889bcce78927639a418192bd9dc6d
RobsBuddySystem <robsbuddysystem@users.noreply.github.com>
2026-07-26 10:17:25 +0200

    2026-07-26: nightly city-guide refresh + Barcelona digest + data regen (auto)

--- comedy-atlas/sw.js
+++ comedy-atlas/sw.js
@@ -23,7 +23,7 @@
  */
 "use strict";
 
-var CACHE_VERSION = "atlas-14231c9e63b8";
+var CACHE_VERSION = "atlas-f4df0a13c3a8";
 var STATIC_CACHE = "atlas-static-" + CACHE_VERSION;
 var DATA_CACHE = "atlas-data-" + CACHE_VERSION;
 
