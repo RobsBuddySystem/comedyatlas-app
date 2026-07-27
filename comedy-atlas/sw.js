04e020cc4c5d503c247f030df758dadf40717283
RobsBuddySystem <robsbuddysystem@users.noreply.github.com>
2026-07-27 19:01:00 +0200

    2026-07-27: publish [sw-bump-nav] 1 file(s) added, 0 removed via publish_worker
    CACHE_VERSION bump so installed service workers pick up the restructured nav
    
    Staged at 2026-07-27T17:00:58.916253+00:00 from git None.
    Scope: ['comedy-atlas/sw.js', 'comedy-atlas/sw.js/*']

--- comedy-atlas/sw.js
+++ comedy-atlas/sw.js
@@ -23,7 +23,7 @@
  */
 "use strict";
 
-var CACHE_VERSION = "atlas-a1cbea580c26";
+var CACHE_VERSION = "atlas-d3578a528062";
 var STATIC_CACHE = "atlas-static-" + CACHE_VERSION;
 var DATA_CACHE = "atlas-data-" + CACHE_VERSION;
 
