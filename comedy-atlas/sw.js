1c2612fcfdea24094d79315d49f5809bc55d8f74
RobsBuddySystem <robsbuddysystem@users.noreply.github.com>
2026-08-09 00:47:57 -0500

    2026-08-09: publish [auto_publish_daily] 18907 file(s) added, 0 removed via publish_worker
    2026-08-09: auto-publish daily | 5994 events, 61 cities
    
    Staged at 2026-08-09T05:45:57.340729+00:00 from git None.
    Scope: ['data/comedy-atlas/countries.json', 'data/comedy-atlas/countries.json/*', 'data/comedy-atlas/cities.json', 'data/comedy-atlas/cities.json/*', 'data/comedy-atlas/organizations.json', 'data/comedy-atlas/organizations.json/*', 'data/comedy-atlas/show_series.json', 'data/comedy-atlas/show_series.json/*', 'data/comedy-atlas/upcoming_events.json', 'data/comedy-atlas/upcoming_events.json/*', 'data/comedy-atlas/public_booking_methods.json', 'data/comedy-atlas/public_booking_methods.json/*', 'data/comedy-atlas/festivals.json', 'data/comedy-atlas/festivals.json/*', 'data/comedy-atlas/tours.json', 'data/comedy-atlas/tours.json/*', 'data/comedy-atlas/opportunities.json', 'data/comedy-atlas/opportunities.json/*', 'data/comedy-atlas/MANIFEST.json', 'data/comedy-atlas/MANIFEST.json/*', 'data/comedy-atlas/moderation_stats.json', 'data/comedy-atlas/moderation_stats.json/*', 'data/comedy-atlas/globe-cities.json', 'data/comedy-atlas/globe-cities.json/*', 'data/comedy-atlas/search_index.json', 'data/comedy-atlas/search_index.json/*', 'data/comedy-atlas/featured_placements.json', 'data/comedy-atlas/featured_placements.json/*', 'data/comedy-atlas/sitemap_lastmod_cache.json', 'data/comedy-atlas/sitemap_lastmod_cache.json/*', 'data/map/cities.geojson', 'data/map/cities.geojson/*', 'data/map/cities/*.json', 'comedy-atlas/*', 'sitemap.xml', 'sitemap.xml/*', 'robots.txt', 'robots.txt/*', 'sitemap_index.xml', 'sitemap_index.xml/*', 'sitemap-blog.xml', 'sitemap-blog.xml/*', 'sitemap-calendar.xml', 'sitemap-calendar.xml/*', 'sitemap-changelog.xml', 'sitemap-changelog.xml/*', 'sitemap-city.xml', 'sitemap-city.xml/*', 'sitemap-comic.xml', 'sitemap-comic.xml/*', 'sitemap-event.xml', 'sitemap-event.xml/*', 'sitemap-festival.xml', 'sitemap-festival.xml/*', 'sitemap-hub.xml', 'sitemap-hub.xml/*', 'sitemap-org.xml', 'sitemap-org.xml/*', 'sitemap-show.xml', 'sitemap-show.xml/*', 'sitemap-venue.xml', 'sitemap-venue.xml/*', 'index.html', 'index.html/*', 'shows.html', 'shows.html/*', 'robertlericain/index.html', 'robertlericain/index.html/*']

--- comedy-atlas/sw.js
+++ comedy-atlas/sw.js
@@ -24,7 +24,7 @@
  */
 "use strict";
 
-var CACHE_VERSION = "atlas-bea98101263d";
+var CACHE_VERSION = "atlas-f9bed518dbc9";
 var STATIC_CACHE = "atlas-static-" + CACHE_VERSION;
 var DATA_CACHE = "atlas-data-" + CACHE_VERSION;
 
